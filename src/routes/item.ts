6afcdbee5b02b87a30f9ee736852f4b2aeda2c10
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2024-01-03 23:00:12 +0530

    Updated item route

---
+++ src/routes/item.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import multer from "multer";
+import { addItem, deleteItem, getItem, getItems } from "../controllers/item";
+import { v4 as uuidv4 } from "uuid";
+
+const router = express.Router();
+
+const itemStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./src/uploads");
+  },
+  filename: (req, file, cb) => {
+    const uniqueFilename = uuidv4();
+    const fileExtension = file.originalname.split(".").pop();
+    const filename = `${uniqueFilename}.${fileExtension}`;
+    cb(null, filename);
+  },
+});
+
+const itemUpload = multer({ storage: itemStorage });
+
+router.get("/", getItems);
+router.get("/:id", getItem);
+router.post("/add", itemUpload.single("file"), addItem);
+router.delete("/delete/:id", deleteItem);
+
+export default router;
--- src/routes/lostItem.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { addLostItem, getByCategory } from "../controllers/lostItem.js";
-const router = express.Router();
-
-router.post("/addLostItem", addLostItem);
-router.get("/getByCategory", getByCategory);
-
-export default router;
