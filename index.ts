00adda86f40a8298d249d802a2223003a185d02a
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2024-01-03 22:59:34 +0530

    Added main file

---
+++ index.ts
@@ -0,0 +1,44 @@
+import express, { NextFunction, Request, Response } from "express";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import cors from "cors";
+import itemRoute from "./src/routes/item";
+import fileRoute from "./src/routes/file";
+
+dotenv.config();
+const app = express();
+const PORT: number = Number(process.env.PORT) || 5000;
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+app.use("/items", itemRoute);
+app.use("/files", fileRoute);
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  const status: number = err.status || 500;
+  const message: string = err.message || "Something went wrong";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
+
+app.use("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "API Working Fine" });
+});
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.MONGO_URL as string)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error: Error) => {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
+  });
